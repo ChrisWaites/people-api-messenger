823a7076a035ca033d307ca23dc637838b327047
Chris Waites <cwaites3@gatech.edu>
2018-05-11 00:19:45 -0400

    Update formatting.

--- messenger/views.py
+++ messenger/views.py
@@ -12,50 +12,54 @@ LOGIN_URL = 'https://people-api-server.herokuapp.com/social-login/'
 REGISTER_URL = 'https://people-api-server.herokuapp.com/register/'
 SERVER_URL = 'https://people-api-server.herokuapp.com/'
 
+
 def index(request):
     if request.method == 'GET':
-        """Before allowing people to message your bot, Facebook has implemented a verify token
-        that confirms all requests that your bot receives came from Facebook.""" 
         token_sent = request.args.get("hub.verify_token")
         if token_sent == settings.VERIFY_TOKEN:
             return request.args.get("hub.challenge")
-        return 'Invalid verification token'
-    else: # POST
+        return HttpResponse('Invalid verification token.')
+
+    else: 
         req = request.get_json()
         print(req)
+
         for event in req.get('entry'):
             messaging = event.get('messaging')
             for message in messaging:
                 if message.get('message'):
+
                     recipient_id = message.get('sender').get('id')
                     text = message.get('message').get('text')
+
                     try:
                         if text == 'help':
                             bot.send_text_message(recipient_id, 'Commands:\n\nregister\nlogin\nlogout')
+                            
                         elif text == 'register':
                             bot.send_button_message(
-                                recipient_id, 'Click here to register.',
-                                    [{
+                                recipient_id, 'Click here to register.', [{
                                         'type': 'web_url',
                                         'url': REGISTER_URL,
                                         'title': 'Register',
                                     }]
                                 )
+
                         elif text == 'login':
                             bot.send_button_message(
-                                recipient_id, 'Click here to login.',
-                                    [{
+                                recipient_id, 'Click here to login.', [{
                                         'type': 'account_link',
                                         'url': LOGIN_URL,
                                     }]
                                 )
+
                         elif text == 'logout':
                             bot.send_button_message(
-                                recipient_id, 'Click here to logout.',
-                                    [{
-                                            'type': 'account_unlink'
+                                recipient_id, 'Click here to logout.', [{
+                                        'type': 'account_unlink'
                                     }]
                                 )
+
                         else:
                             bot.send_text_message(recipient_id, 'Sample Query? [1-5]')
 
@@ -63,4 +67,5 @@ def index(request):
                         print(e)
                         bot.send_text_message(recipient_id, 'Sorry, something must have gone wrong.')
 
-    return HttpResponse('Message processed')
+        return HttpResponse('Message processed.')
+
